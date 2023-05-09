be1b4f08a9eb6ea8af1e43f1a86f927f601feb91
AbassKoyang <abasskoyang12345@gmail.com>
2023-05-09 23:16:42 +0100

    MY DREEMZ REPO

---
+++ jsfolder/works.js
@@ -0,0 +1,138 @@
+let menu = document.querySelector('#menu__icon')
+let nav__list = document.querySelector('.nav__list')
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x')
+    nav__list.classList.toggle('open');
+}
+
+// Pop up close and Open
+
+let popup = document.getElementById("popup");
+
+function openPopup(){
+    popup.classList.add("open-popup");
+}
+
+function closePopup(){
+    popup.classList.remove("open-popup");
+}
+
+
+
+
+
+
+
+
+
+
+let popup2 = document.getElementById("popup2");
+
+function openPopup2(){
+    popup2.classList.add("open-popup");
+}
+
+function closePopup2(){
+    popup2.classList.remove("open-popup");
+}
+
+
+
+
+
+let popup3 = document.getElementById("popup3");
+
+function openPopup3(){
+    popup3.classList.add("open-popup");
+}
+
+function closePopup3(){
+    popup3.classList.remove("open-popup");
+}
+
+
+
+
+
+let popup4 = document.getElementById("popup4");
+
+function openPopup4(){
+    popup4.classList.add("open-popup");
+}
+
+function closePopup4(){
+    popup4.classList.remove("open-popup");
+}
+
+
+
+
+
+let popup5 = document.getElementById("popup5");
+
+function openPopup5(){
+    popup5.classList.add("open-popup");
+}
+
+function closePopup5(){
+    popup5.classList.remove("open-popup");
+}
+
+
+
+
+
+
+
+let popup6 = document.getElementById("popup6");
+
+function openPopup6(){
+    popup6.classList.add("open-popup");
+}
+
+function closePopup6(){
+    popup6.classList.remove("open-popup");
+
+}
+
+
+
+
+
+let popup7 = document.getElementById("popup7");
+
+function openPopup7(){
+    popup7.classList.add("open-popup");
+}
+
+function closePopup7(){
+    popup7.classList.remove("open-popup");
+
+}
+
+
+
+let popup8 = document.getElementById("popup8");
+
+function openPopup8(){
+    popup8.classList.add("open-popup");
+}
+
+function closePopup8(){
+    popup8.classList.remove("open-popup");
+
+}
+
+
+
+let popup9 = document.getElementById("popup9");
+
+function openPopup9(){
+    popup9.classList.add("open-popup");
+}
+
+function closePopup9(){
+    popup9.classList.remove("open-popup");
+
+}
